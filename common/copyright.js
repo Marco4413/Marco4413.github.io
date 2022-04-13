f25560fa21cc6f5450c387beaaaa92dd44bea075
Marco <46133335+Marco4413@users.noreply.github.com>
2022-04-13 12:35:28 +0200

    Added copyright.js

---
+++ common/copyright.js
@@ -0,0 +1,18 @@
+/* MIT Copyright (c) 2022 [hds536jhmk](https://github.com/hds536jhmk/hds536jhmk) */
+/* This script appends a copyright notice with a ref url to the document's body */
+
+const script = document.currentScript;
+window.addEventListener("load", () => {
+    const urlMatcher = /^(?:https?:\/\/)?((?:www\.)?([^.]+)\.[^/]*)\/?.*$/g;
+    const [ _, rootDocumentUrl, ioGithubName ] = urlMatcher.exec(window.location.href);
+    
+    const url = script.attributes.getNamedItem("url")?.value ?? "/";
+    const name = script.attributes.getNamedItem("name")?.value ?? ioGithubName;
+    const year = script.attributes.getNamedItem("year")?.value ?? (new Date().getFullYear());
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.innerText = `Copyright (c) ${year} ${name}`;
+    a.classList.add("copyright")
+    document.body.appendChild(a);
+});
