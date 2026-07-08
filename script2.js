e39d166af9e9997283ae98ddece481803198a596
Marco <46133335+Marco4413@users.noreply.github.com>
2026-07-08 19:21:27 +0200

    Updated __PageToMarkdown()

--- script2.js
+++ script2.js
@@ -3,22 +3,36 @@ import { MarkdownBuilder } from "https://Marco4413.github.io/MarkdownBuilder/Mar
 window.addEventListener("load", () => {
     const markdownOptions = {
         baseUrl: new URL(location.href),
+        /** @param {HTMLImageElement} $img */
         getImageStyle: $img => {
-            if ($img.classList.contains("tool-logo")) {
-                return { width: 16 };
-            }
-            return {};
+            const style = { width: 192 };
+
+            const isAvatar = $img.parentElement?.classList.contains("avatar-container") ?? false;
+            if (isAvatar) style.width = 48;
+            if ($img.classList.contains("tool-logo")) style.width = 16;
+            return style;
         },
     };
 
     window.__PageToMarkdown = () => {
         const builder = new MarkdownBuilder(markdownOptions);
-        return builder.OverrideElementWriter("div", writer => (
+        return builder
+            .SetElementWriter("input", self => self)
+            .OverrideElementWriter("div", writer => (
                 /** @param {HTMLDivElement} $el */
                 (self, $el) => {
-                    const isPrecededByHeading = $el.previousElementSibling?.tagName?.match(/^h[0-9]+$/i) !== null;
+                    if ($el.hasAttribute("vr")) return self.WriteText("|");
+
                     const isProject = $el.classList.contains("project-container");
-                    if (isProject && !isPrecededByHeading) self.WriteSeparator();
+                    if (isProject) {
+                        const isPrecededByHeading = $el.previousElementSibling?.tagName?.match(/^h[0-9]+$/i) !== null;
+                        if (!isPrecededByHeading) self.WriteSeparator();
+                        return writer(self, $el);
+                    }
+
+                    const isPageNameContainer = $el.classList.contains("page-name-container");
+                    if (isPageNameContainer) return self.Write($el);
+
                     return writer(self, $el);
                 }
             ))
